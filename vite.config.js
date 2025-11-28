415537a5931d57859f7fa935c15939741917c95a
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2025-11-28 15:41:35 +0700

    added js obfuscated

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,48 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import JavaScriptObfuscator from 'javascript-obfuscator'
+
+const obfuscatePlugin = (options = {}) => ({
+  name: 'vite-javascript-obfuscator',
+  apply: 'build',
+  enforce: 'post',
+  generateBundle(_options, bundle) {
+    Object.keys(bundle).forEach((fileName) => {
+      const chunk = bundle[fileName]
+      if (chunk?.type === 'chunk') {
+        const obfuscated = JavaScriptObfuscator.obfuscate(chunk.code, options)
+        chunk.code = obfuscated.getObfuscatedCode()
+      }
+    })
+  }
+})
+
+const enableObfuscation = process.env.VITE_DISABLE_OBFUSCATION !== 'true'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    enableObfuscation
+      ? obfuscatePlugin({
+          compact: true,
+          controlFlowFlattening: true,
+          deadCodeInjection: true,
+          disableConsoleOutput: true,
+          identifierNamesGenerator: 'hexadecimal',
+          renameGlobals: false,
+          rotateStringArray: true,
+          selfDefending: true,
+          simplification: true,
+          stringArray: true,
+          stringArrayEncoding: ['rc4'],
+          stringArrayThreshold: 0.75,
+          target: 'browser'
+        })
+      : null
+  ].filter(Boolean),
   build: {
     sourcemap: false,
-  },
+    minify: 'esbuild'
+  }
 })
 
